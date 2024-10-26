1d9348f23e4855a1470fa3ea8dd113a4ffa934e2
XeRxEs02 <158118278+XeRxEs02@users.noreply.github.com>
2024-10-26 19:46:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,7 @@ import cors from 'cors';
 const app=express();
 app.use(express.json());
 app.use(cors());
-//app.use(
-  //  cors({
-    //origin:'http://localhost:5555',
-    //methods:['GET','POST','PUT','DELETE'],
-    //allowedHeaders:['Content-Type'],
-//}));
+
 app.get('/',(request,response)=>{
     console.log(request);
     return response.status(234).send('HEY THERE WELCOME!');
